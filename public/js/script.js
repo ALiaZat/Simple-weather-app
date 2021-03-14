f320de9624c312d9510dafdb0dd57729fd193a39
Ahmad Shnaiwer <ashnaiwer@apa.org>
2021-03-14 12:56:17 +0200

    Fixed script

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/books/math?id=1233').then((response) => {
+fetch('/books/math?id=1233').then((response) => {
     response.json().then((data) => {
         console.log(data);
     })
